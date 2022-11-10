dbe52f01e9afc0a6589af20e806b5e8d8065ba95
Mark Wilson <toques-nightly.0c@icloud.com>
2022-11-10 08:31:26 +0000

    Difficulty levels

--- game.py
+++ game.py
@@ -1,8 +1,14 @@
+import random
 from traverse_tree import *
 from players import *
 
 MINIMAX = 0
 RANDOM = 1
+
+
+def decision(difficulty):
+    probability = difficulty / 5
+    return random.random() < probability
 
 
 def comp_vs_comp():
@@ -25,27 +31,40 @@ def comp_vs_comp():
 
 def play():
     computer_symbol = NOUGHT
-    computer_engine = MINIMAX   # RANDOM or MINIMAX
     comp_plays_first = False
+    difficulty = 3  # 0->5 0=random 5=unbeatable
+
+    while True:
+        str_difficulty = input('\nChoose difficulty: [0..5 where 0=random 5=unbeatable] ')
+        try:
+            difficulty = int(str_difficulty)
+        except ValueError:
+            print('0..5 only please')
+            continue
+        if 0 <= difficulty <= 5:
+            break
+        else:
+            print('0..5 only please')
 
     game_node = node(computer_symbol)
 
-    if computer_engine == MINIMAX:
-        computer = minimax_player(game_node)
-    elif computer_engine == RANDOM:
-        computer = random_player(game_node)
+    comp_minimax = minimax_player(game_node)
+    comp_random = random_player(game_node)
 
     human = human_player(game_node)
 
     game_node.print_position()
 
     if comp_plays_first:
-        computer.random_move()
+        comp_random.make_move()
         game_node.print_position()
 
     while True:
         human.make_move()
-        computer.make_move()
+        if decision(difficulty):
+            comp_minimax.make_move()
+        else:
+            comp_random.make_move()
 
 
 if __name__ == '__main__':
